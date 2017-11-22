373329e0ed44913bb8ef263133588e23dd4154b8
Edward Barber <ebarber5969@conestogac.on.ca>
2017-11-22 12:07:31 -0500

    Added logic for ball clock but needs fixing

--- Time and motion/VM.cs
+++ Time and motion/VM.cs
@@ -6,6 +6,7 @@ using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Windows;
 
 namespace Time_and_motion
 {
@@ -21,21 +22,21 @@ namespace Time_and_motion
         private const string EMPTY_FILE_ERROR = "Please add values to your file!";
         private const string TERMINATION_AT_BEGINNING_OF_FILE_ERROR = "File has been terminated at the beginning. Was this intentional?";
         private const int MIN_LIST_COUNTER_VALUE = 0;
-
-        // Seperate unused const
-        private const int MINUTES_IN_A_DAY = 1440;
+        private const int MIN_INDICATOR_COUNT = 0;
         private const int MAX_MINUTE_INDICATOR_CAPACITY = 4;
         private const int MAX_FIVE_MINUTE_INDICATOR_CAPACITY = 11;
         private const int MAX_HOUR_INDICATOR_CAPACITY = 12;
+        private const int MAX_HOUR_INDICATOR_CAPACITY_OFFSET = 1;
+        private const int MINUTES_IN_A_DAY = 1440;
 
         private string filePath;
 
         // Seperate unused vars
-        private int ballQueueCount;
-        private int minuteIndicatorCount;
-        private int fiveMinuteIndicatorCount;
-        private int hourIndicatorCount;
-        private int days;
+        //private int ballQueueCount;
+        //private int minuteIndicatorCount;
+        //private int fiveMinuteIndicatorCount;
+        //private int hourIndicatorCount;
+        //private int days;
 
         public string FilePath
         {
@@ -55,6 +56,59 @@ namespace Time_and_motion
                     // if minute indicator has 4 balls then remove 4 balls and add one and one to the five-minute indicator
                     // if five minute indicator has 11 balls then remove 11 balls and add one to the hour indicator 
                     // if hour indicator has 12 balls then remove 12 balls
+
+                    int ballQueueCount = startingBallQueue;
+                    int minuteIndicatorCount = MIN_INDICATOR_COUNT;
+                    int fiveMinuteIndicatorCount = MIN_INDICATOR_COUNT;
+                    int hourIndicatorCount = MIN_INDICATOR_COUNT;
+                    int days = 0;
+                    int minutes = 0;
+
+                    do
+                    {
+                        ballQueueCount--;   // comment out to test
+
+                        if (minuteIndicatorCount >= MAX_MINUTE_INDICATOR_CAPACITY)
+                        {
+                            minuteIndicatorCount -= MAX_MINUTE_INDICATOR_CAPACITY;
+                            ballQueueCount += MAX_MINUTE_INDICATOR_CAPACITY;
+                            fiveMinuteIndicatorCount++;
+                        }
+                        else
+                        {
+                            minuteIndicatorCount++;
+                        }
+
+                        if (fiveMinuteIndicatorCount >= MAX_FIVE_MINUTE_INDICATOR_CAPACITY)
+                        {
+                            fiveMinuteIndicatorCount -= MAX_FIVE_MINUTE_INDICATOR_CAPACITY;
+                            ballQueueCount += MAX_FIVE_MINUTE_INDICATOR_CAPACITY;
+                            hourIndicatorCount++;
+                        }
+                        else
+                        {
+                            fiveMinuteIndicatorCount++;
+                        }
+
+                        if (hourIndicatorCount >= MAX_HOUR_INDICATOR_CAPACITY)
+                        {
+                            hourIndicatorCount -= MAX_HOUR_INDICATOR_CAPACITY;
+                            ballQueueCount += MAX_HOUR_INDICATOR_CAPACITY;// + MAX_HOUR_INDICATOR_CAPACITY_OFFSET;
+                        }
+                        else
+                        {
+                            hourIndicatorCount++;
+                        }
+
+                        minutes++;
+
+                        if (minutes == MINUTES_IN_A_DAY)
+                        {
+                            days++;
+                        }
+                    } while (ballQueueCount != startingBallQueue);
+
+                    MessageBox.Show(startingBallQueue + " balls cycle after " + days + " days.");
                 }
             }
             catch
